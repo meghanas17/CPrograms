2a87c5964528357a0fb6e683b509ff576bb4f74e
meghanas17 <167081767+meghanas17@users.noreply.github.com>
2024-10-21 18:40:47 +0530

    PascalTriangle.c

---
+++ PascalTriangle.c
@@ -0,0 +1,34 @@
+// C program to print the pascal's triangle pattern
+#include <stdio.h>
+
+int main()
+{
+    int rows = 5;
+
+    // outer loop for rows
+    for (int i = 1; i <= rows; i++) {
+
+        // inner loop 1 for leading white spaces
+        for (int j = 0; j < rows - i; j++) {
+            printf(" ");
+        }
+
+        int C = 1; // coefficient
+
+        // inner loop 2 for printing numbers
+        for (int k = 1; k <= i; k++) {
+            printf("%d ", C);
+            C = C * (i - k) / k;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+Output:
+
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
